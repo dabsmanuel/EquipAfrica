055664cbb7c34f84bff3a599b1389aa0ef395319
Emmanuel Oyibo <75739167+dabsmanuel@users.noreply.github.com>
2022-09-21 01:47:28 +0100

    changed file location bootstrpa css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import Navbar from './components/Navbar/Navbar'
-import Header from './components/Header/Header';
-import About from './components/About/About';
-import Services from './components/services/Services';
-import Team from './components/Team/Team';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Services from "./components/services/Services";
+import Team from "./components/Team/Team";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Header/>
-      <About/>
+      <Header />
+      <About />
       <Services />
       <Team />
     </div>
